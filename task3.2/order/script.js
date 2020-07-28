abbe3fdca4479bbcc6ed1616f6ae1ff7a2744ae2
lemospace <thelemospace@gmail.com>
2020-07-28 23:43:07 +0300

    commit

--- task3.2/order/script.js
+++ task3.2/order/script.js
@@ -203,9 +203,66 @@ function setSuccessFor(input) {
 }
 
 var number = document.querySelectorAll(".number");
-number.addEventListener("keypress", function (e) {
-  var key = e.which;
-  if (key < 48 || key > 57) {
-    e.preventDefault();
+number.forEach((number) => {
+  number.addEventListener("keypress", function (e) {
+    var key = e.which;
+    if (key < 48 || key > 57) {
+      e.preventDefault();
+    }
+  });
+});
+
+var monthtext = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sept",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+function populatedropdown(dayfield, monthfield, yearfield) {
+  var today = new Date();
+  var dayfield = document.getElementById(dayfield);
+  var monthfield = document.getElementById(monthfield);
+  var yearfield = document.getElementById(yearfield);
+  for (var i = 0; i < 31; i++) dayfield.options[i] = new Option(i + 1, i + 1);
+  dayfield.options[today.getDate() - 1].selected = true;
+  for (var m = 0; m < 12; m++)
+    monthfield.options[m] = new Option(monthtext[m], monthtext[m]);
+  monthfield.options[today.getMonth()].selected = true;
+  var thisyear = today.getFullYear();
+  for (var y = 0; y < 20; y++) {
+    yearfield.options[y] = new Option(thisyear, thisyear);
+    thisyear += 1;
+  }
+  yearfield.options[0] = new Option(
+    today.getFullYear(),
+    today.getFullYear(),
+    true,
+    true
+  ); //select today's year
+}
+onload = function () {
+  populatedropdown("d", "m", "y");
+};
+
+const submitBtn = document.querySelector("#submit");
+
+submitBtn.addEventListener("click", function () {
+  if (function checkInputs = setSuccess) {
+    localStorage.setItem("username", username.value);
+
+    localStorage.setItem("password", password.value);
+    localStorage.setItem("password2", password2.value);
   }
 });
+
+function myFunction() {
+  location.href = "result.html";
+}
