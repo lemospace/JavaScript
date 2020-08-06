3181fa5738e696adac6083cfbf2eb7277795bc89
lemospace <thelemospace@gmail.com>
2020-08-06 18:12:59 +0300

    commit

--- task3.5/weather.js
+++ task3.5/weather.js
@@ -1,8 +1,6 @@
 var x = document.getElementById("latitude");
 var y = document.getElementById("longitude");
-
-var latValue = x.value;
-var lonValue = y.value;
+var WeatherAPIKey = "62adbc2bffd1bb07adc82799df6242b9";
 
 function getLocation() {
   if (navigator.geolocation) {
@@ -13,18 +11,20 @@ function getLocation() {
 }
 
 function showPosition(position) {
-  x.innerHTML = position.coords.latitude;
-  y.innerHTML = position.coords.longitude;
+  var lat = position.coords.latitude;
+  var lon = position.coords.longitude;
+  var weatherLLQueryURL =
+    "http://api.openweathermap.org/data/2.5/weather?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&appid=" +
+    WeatherAPIKey;
 }
-
-var WeatherAPIKey = "62adbc2bffd1bb07adc82799df6242b9";
-var weatherLLQueryURL =
-  "http://api.openweathermap.org/data/2.5/weather?lat=" +
-  latValue +
-  "&lon=" +
-  lonValue +
-  "&appid=" +
-  WeatherAPIKey;
-console.log(weatherLLQueryURL);
+function weatherBaloon(byLocation) {
+  fetch(weatherLLQueryURL)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
 
 //{ "id": 706483, "name": "Kharkiv", "state": "", "country": "UA", "coord": { "lon": 36.25, "lat": 50.0 } },
